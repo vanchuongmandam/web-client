36d32356cdbd5b640ea858f33994b99a8614a3b7
longathelstan <121792617+longathelstan@users.noreply.github.com>
2025-08-05 12:38:23 +0700

    nah the last update

--- next.config.ts
+++ next.config.ts
@@ -18,30 +18,6 @@ const nextConfig: NextConfig = {
     ],
     dangerouslyAllowSVG: true,
   },
-  
-  webpack: (config, { isServer }) => {
-   
-    if (isServer) {
-      config.resolve.fallback = {
-   
-        fs: false,            // File system module
-        stream: false,        // Stream module
-        tls: false,           // TLS (Transport Layer Security) module
-        net: false,           // Network module
-        zlib: false,          // Compression module (zlib)
-        http: false,          // HTTP client/server module
-        http2: false,         // HTTP/2 protocol module
-        dns: false,           // DNS module
-        os: false,            // Operating System module
-        path: false,          // Path manipulation module
-        querystring: false,   // Query string parsing module
-        crypto: false,
-        util: false,
-      };
-    }
-
-    return config;
-  },
 
 };
 
